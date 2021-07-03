2f2fbf129f8e4becd38ec40b99725062be6550ec
Ayoub Benaceur <ayoubb2121@gmail.com>
2021-07-03 01:12:52 +0100

    responsive main page UI tweaks

--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -5,9 +5,17 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>SSMT</title>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Roboto&display=swap" rel="stylesheet">
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
     <style>
+
+        *{
+            font-family: 'Roboto', sans-serif;
+        }
+
         .background-img {
             background: url('img/background.png');
             background-repeat: no-repeat;
@@ -20,7 +28,6 @@
             appearance :none;
             background-color: rgba(255, 255, 255, 75%);
             border: none;
-            padding: 0.5rem 1rem;
             margin: 0;
             width: 100%;
             font-family: inherit;
@@ -39,20 +46,52 @@
             color:black;
             opacity :0.9;
         }
+        .animate-down{
+            top : 0px;
+            animation-name: down;
+            animation-duration: 1s;
+        }
+        .animate-up{
+            top : -70px;
+            animation-name: up;
+            animation-duration: 1s;
+        }
+        @keyframes down{
+            from {top: -70px;}
+            to {top: 0px;}
+        }
+        @keyframes up{
+            from {top: 0px;}
+            to {top: -70px;}
+        }
+        #logo{
+            image-rendering: pixelated;
+            image-rendering: -moz-crisp-edges;         /* Firefox */
+            image-rendering:   -o-crisp-edges;         /* Opera */
+            image-rendering: -webkit-optimize-contrast;/* Webkit (non-standard naming) */
+            image-rendering: crisp-edges;
+            -ms-interpolation-mode: nearest-neighbor;
+            max-width: auto;
+            height: 90%;
+        }
+        @media (min-width: 640px) { 
+            * {
+            }
+        }
     </style>
 </head>
 
-<body class="relative h-full">
+<body class=" relative h-full">
     <div class="h-screen flex-col">
         <div class="absolute text-white">
         </div>
         <div class="absolute h-screen w-screen flex-wrap flex justify-center content-center">
             <div class=" h-auto flex justify-center" style="width: 60%;">
-                <form class="flex justify-center" style="width: 100%;" action="/search" method="POST">
+                <form class="flex flex-col md:flex-row lg:flex-row justify-center w-full" action="/search" method="POST">
                     @csrf
-                    <div class="relative inline mr-2 " style="width: 40%;">
-                        <input onclick="showSelect1()"  readonly placeholder="Qu'est-ce que tu cherches ?" class="select shadow-lg rounded py-2 px-5" type="text" name="select1" id="select1">
-                        <div id="select1Elements" style="display:none;" class="bg-opacity-85 w-full bg-white absolute top-12 right-0 rounded">
+                    <div class="z-10 relative inline mr-2 p-0 w-full mb-6 md:mb-0 lg:mb-0 md:w-2/5 text-md " >
+                        <input onclick="showSelect1()"  readonly placeholder="Qu'est-ce que tu cherches ?" class="select shadow-lg rounded py-2  px-5" type="text" name="select1" id="select1">
+                        <div id="select1Elements" style="display:none;" class="bg-opacity-85 w-full bg-white absolute top-12 md:top-12 lg:top-12 right-0 rounded">
                             <ul>
                                 <li onclick="select1('Offre d\'emploi')" class="border-b cursor-pointer hover:bg-gray-100 pl-4 pb-2 pt-2 rounded ">Offre d'emploi</li>
                                 <li onclick="select1('Offre de stage')" class="border-b cursor-pointer hover:bg-gray-100 pl-4 pb-2 pt-2 rounded ">Offre de stage</li>
@@ -61,16 +100,16 @@
                         </div>
                     </div>
 
-                    <div class="relative inline mr-2 " style="width: 40%;">
-                        <input onclick="showSelect2()" readonly placeholder="Quel département ?" class="select shadow-lg rounded py-2 px-5" type="text" name="select2" id="select2">
-                        <div  id="select2Elements" class="overflow-y-auto max-h-60 bg-opacity-100 w-full bg-white absolute top-12 right-0 rounded">
+                    <div class="relative inline mr-2 w-full mb-8 md:mb-0 lg:mb-0 md:w-2/5 text-md" >
+                        <input onclick="showSelect2()" readonly placeholder="Quel département ?" class="select shadow-lg rounded py-2  px-5" type="text" name="select2" id="select2">
+                        <div  id="select2Elements" class="overflow-y-auto max-h-60 bg-opacity-100 w-full bg-white absolute top-12 md:top-12 lg:top-12 right-0 rounded">
                             <ul>
                                 
                             <ul>
                         </div>
                     </div>
                     
-                    <button onclick="" class="shadow-lg bg-yellow-500 hover:bg-yellow-400 focus:shadow-outline focus:outline-none font-bold text-white rounded py-2 px-auto " type="submit" value="Lancer" style="width: 20%;">
+                    <button onclick="" class="w-full md:w-1/5 lg:w-1/5 shadow-lg bg-yellow-500 hover:bg-yellow-400 focus:shadow-outline focus:outline-none font-bold text-white rounded py-2 px-auto " type="submit" value="Lancer">
                         Lancer
                     </button>
                 </form>
@@ -78,11 +117,21 @@
         </div>
         <div class="background-img text-white ">
         </div>
+        <div id="navbar" class="top-0 absolute text-white bg-opacity-70 bg-black w-full" style="height: 7%;">
+            <div class="w-full h-full flex justify-center mt-auto">
+                <div class="flex justify-between w-9/12 h-full mt-auto mb-auto">
+                    <img class="mt-auto mb-auto" id="logo" src="img/logo.png">
+                    <div class = "cursor-pointer mt-auto mb-auto">
+                        <a class="">À propos</a>
+                    </div>
+                </div>
+            </div>
+        </div>
         <div class="text-white bg-black w-full" style="height: 4%;">
             <div class="h-full flex justify-center mt-auto">
-                <div class="mt-auto mb-auto">
-                    <span class="font-medium mr-2">ADRESSE</span>
-                    <span>Imm Larki, 2eme Etage, Blachache M'haita - Taroudannt</span>
+                <div class="text-xs md:text-sm lg:text-base mt-auto mb-auto">
+                    <span class=" mr-2">ADRESSE</span>
+                    <span>Imm Larki, 2eme Etage, Blachache M'haita - Taroudant</span>
                 </div>
             </div>
         </div>
@@ -173,6 +222,7 @@
         Select2.value = "";
         checkChoise();
     }
+
     function loadSelect2(elements){
         let ul = select2Elements.firstElementChild;
         let child = ul.firstElementChild; 
@@ -193,6 +243,31 @@
             ul.appendChild(li);
         })
     }
+    
+    {{-- let firstAnimation = true;
+    let navbarDown = false;
+    function animate(e){
+        let navbar = document.getElementById("navbar");
+        if(e.pageY <= 120){
+            if(firstAnimation == false){
+                navbarDown = true;
+                navbar.className = "-top-20 animate-down absolute text-white bg-opacity-50 bg-black w-full";
+                window.removeEventListener('mousemove',animate);
+                setTimeout(()=>{ window.addEventListener('mousemove',animate); }, 1000);
+            }  
+        }
+        else if(e.pageY > 120){
+            if(firstAnimation == false && navbarDown == true){
+                navbarDown = false;
+                navbar.className = "-top-20 animate-up absolute text-white bg-opacity-50 bg-black w-full";
+
+            }
+            firstAnimation  = false;            
+        }
+          
+    } --}}
+
+    window.addEventListener('mousemove',animate);
 </script>
 
 </html>
