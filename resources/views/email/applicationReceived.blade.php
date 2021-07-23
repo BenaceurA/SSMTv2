5b5e9dc156bd33b64378a203040009c7eb3d2d64
Ayoub Benaceur <ayoubb2121@gmail.com>
2021-07-23 13:09:35 +0100

    email change

--- resources/views/email/applicationReceived.blade.php
+++ resources/views/email/applicationReceived.blade.php
@@ -1,11 +1,21 @@
-<p>Bonjour @if($Sexe == "Masculin")M.@elseif($Sexe == "Féminin")Mme.@endif {{$Nom_Prenom}},</p>
+<p style="color:black;font-size:18px;">Bonjour @if($Sexe == "Masculin")M.@elseif($Sexe == "Féminin")Mme.@endif {{$Nom_Prenom}},</p>
 
-<p>Nous avons bien reçu votre candidature et nous vous remercions pour la confiance que vous témoignez à notre société SSMT.
+<p style="color:black;font-size:18px;">Nous avons bien reçu votre candidature et nous vous remercions pour la confiance que vous témoignez à notre société SSMT.
 Nous allons étudier votre dossier et revenir vers vous. Sans réponse de notre part dans un délai de 1 mois, veuillez considérer votre candidature non retenue pour le poste à pourvoir.
 Nous nous permettons de garder votre CV dans notre vivier sauf avis contraire de votre part et ne manquerons pas à vous contacter dès qu’une nouvelle opportunité correspondante à votre profil se présente.</p>
 
-<p>Cordialement</p>
+<p style="color:black;font-size:18px;">Cordialement</p>
 
-<p>Direction des Ressources Humaines, SSMT</p>
+<p style="color:black;font-size:18px;">Direction des Ressources Humaines, SSMT</p>
 
-<p>(Ceci est un message du système qui est généré automatiquement, merci de ne pas répondre à cet email.)</p>
+<p style="font-weight:bold;color:#558ed5;">(Ceci est un message du système qui est généré automatiquement, merci de ne pas répondre à cet email.)</p>
+
+
+<a href="#"><img style="width: 130px;" src="{{ asset('img/logo.png') }}"></a>
+
+<p style="font-weight:bold;color:green;">Service RH SSMT</p>
+<p style="font-size:12px;color:green;">E-Mail  : <a href="mailto:recrutement@ssmt.ma">recrutement@ssmt.ma</a></p>
+<p style="font-size:12px;color:green;">Site  : <a href="https://www.ssmt.ma">www.ssmt.ma</a></p>
+<p style="font-weight:bold;color:green;">Afin de contribuer au respect de l'environnement, merci de n'imprimer ce mail qu'en cas de nécessité.</p>
+
+<p style="font-size:11px;">Ce message et toutes les pièces jointes sont établis à l' attention exclusive de ses destinataires et sont confidentiels. Si vous recevez ce message par erreur, merci de le détruire et d'en avertir immédiatement l' expéditeur. Toute utilisation de ce message non conforme à sa destination, toute diffusion ou toute publication, totale ou partielle, est interdite, sauf autorisation expresse. L' internet ne permettant pas d' assurer l' intégrité de ce message, La Sté SSMT décline toute responsabilité au titre de ce message, dans l' hypothèse ou il aurait été modifié.</p>
