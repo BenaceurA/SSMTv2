c318b6019c0e128a644bb8858a033a58c0a2317e
Ayoub Benaceur <ayoubb2121@gmail.com>
2021-08-28 06:08:03 +0100

    asset('css/app.css',true)

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -14,7 +14,7 @@
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Rubik:wght@400;500;600&display=swap" rel="stylesheet">
 
-    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <link href="{{ asset('css/app.css',true) }}" rel="stylesheet">
 
     <style>
         *{
