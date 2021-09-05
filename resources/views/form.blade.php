ee3c0dee68b3ebdd69801925386d109402485253
Ayoub Benaceur <ayoubb2121@gmail.com>
2021-09-05 18:01:27 +0100

    fixed font-family issue

--- resources/views/form.blade.php
+++ resources/views/form.blade.php
@@ -8,16 +8,16 @@
     <title>Document</title>
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@500&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500&display=swap" rel="stylesheet">
 
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Rubik:wght@400;500;600&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Rubik:wght@300;400;500;600&display=swap" rel="stylesheet">
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
     <style>
+    
         *{
-            font-family: 'Rubik','Roboto', sans-serif;
-             
+            
         }
         .background-img {
             background: url('../img/background.png');
@@ -45,10 +45,10 @@
 </head>
 
 <body class="relative h-screen background-img">
-    <div class="text-sm md:text-sm lg:text-base pb-8 pt-16 flex justify-center ">
-         @if($type == "emploi") @include("formemploi");
-         @elseif($type == "stage") @include("formstage");
-         @elseif($type == "spontaneous") @include("formspontaneous"); @endif
+    <div id="form" class="text-sm md:text-sm lg:text-base pb-16 pt-16 sm:pt-24 flex justify-center ">
+         @if($type == "emploi") @include("formemploi")
+         @elseif($type == "stage") @include("formstage")
+         @elseif($type == "spontaneous") @include("formspontaneous") @endif
     </div>
     @include("top-bar")
     @include("bottom-bar")
