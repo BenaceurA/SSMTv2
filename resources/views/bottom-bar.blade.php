7c64fbef9d5ec1a7e75dd72b90249ac633f34076
Ayoub Benaceur <ayoubb2121@gmail.com>
2021-09-05 19:39:03 +0100

    added xss fontsize

--- resources/views/bottom-bar.blade.php
+++ resources/views/bottom-bar.blade.php
@@ -1,18 +1,18 @@
 <div class="bottom-0 absolute text-white bg-black w-full bg-opacity-30" style="height: 7vh;font-family: 'Rubik','Roboto', sans-serif;">
     <div class="h-full flex justify-around mt-auto">
-        <div class="flex flex-col sm:font-semibold text-xs md:text-sm lg:text-sm mt-auto mb-auto">
+        <div class="flex flex-col sm:font-semibold text-xss md:text-sm lg:text-sm mt-auto mb-auto">
             <div class="text-center">TELEPHONE</div>
             <div class="text-center">0528551098</div>
         </div>
-        <div class="flex flex-col sm:font-semibold text-xs md:text-sm lg:text-sm mt-auto mb-auto">
+        <div class="flex flex-col sm:font-semibold text-xss md:text-sm lg:text-sm mt-auto mb-auto">
             <div class="text-center">FAX</div>
             <div class="text-center">0528551704</div>
         </div>
-        <div class="flex flex-col sm:font-semibold text-xs md:text-sm lg:text-sm mt-auto mb-auto">
+        <div class="flex flex-col sm:font-semibold text-xss md:text-sm lg:text-sm mt-auto mb-auto">
             <div class="text-center">ADRESSE</div>
             <div class="text-center">Imm Larki, 2eme Etage, Blachache M'haita - Taroudant</div>
         </div>   
-        <div class="flex flex-col sm:font-semibold text-xs md:text-sm lg:text-sm mt-auto mb-auto">
+        <div class="flex flex-col sm:font-semibold text-xss md:text-sm lg:text-sm mt-auto mb-auto">
             <div class="text-center">EMAIL</div>
             <div class="text-center">info@ssmt.ma</div>
         </div>       
